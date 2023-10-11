2c8ac135aa52d6284a9a5c1b7556c9f1c4afec46
Reynaldo Moreno <reynaldomoreno173@gmail.com>
2023-10-10 23:31:20 -0400

    create method get for endpoint products and categories

--- index.js
+++ index.js
@@ -1,1 +1,60 @@
-console.log('Hello World');
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+app.get('/products', (req, res) => {
+  res.json([
+    {
+      name: 'Product 1',
+      price: 1000,
+    },
+    {
+      name: 'Product 2',
+      price: 2000,
+    },
+  ]);
+});
+
+app.get('/products/:id', (req, res) => {
+  const { id } = req.params;
+  res.json({
+    id,
+    name: 'product 1',
+    price: 1000,
+  });
+});
+
+app.get('/categories', (req, res) => {
+  const { categoryId } = req.params;
+
+  res.json([
+    {
+      categoryId,
+      name: 'Food',
+      products: [],
+    },
+    {
+      categoryId,
+      name: 'Drink',
+      products: [],
+    },
+  ]);
+});
+
+app.get('/categories/:categoryId', (req, res) => {
+  const { categoryId } = req.params;
+
+  res.json({
+    categoryId,
+    categori: 'Food',
+    products: [],
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
